150db8a3eb9bba42b2c7ec0ed03fbb3404941edf
Takanori Ishikawa <takanori.ishikawa@gmail.com>
2016-10-15 16:00:13 +0900

    Add Function definition

--- lib/chelsy/ast.rb
+++ lib/chelsy/ast.rb
@@ -1,4 +1,5 @@
 require "chelsy/syntax"
+require 'forwardable'
 
 module Chelsy
 
@@ -8,11 +9,75 @@ module Chelsy
   class Element < Node
   end
 
+  class Type < Element
+    def initialize(const: false, restrict: false, volatile: false)
+      @const = !!const
+      @restrict = !!restrict
+      @volatile = !!volatile
+    end
+
+    def const?;    @const end
+    def restrict?; @restrict end
+    def volatile?; @volatile end
+  end
+
+  class Declaration < Element
+  end
+
+  class Definition < Element
+    def initialize(extern: false, static: false)
+      @extern = !!extern
+      @static = !!static
+    end
+
+    def extern?; @extern end
+    def static?; @static end
+  end
+
   class Expr < Element
   end
 
+  class Stmt < Element
+  end
+
   module Syntax
-    Expr = Any.new('Expression', [Expr, Symbol])
+    Ident = Any.new('Identifier', [Symbol])
+    Expr  = Any.new('Expression', [Expr, Symbol])
+    Type  = Any.new('TypeSpecifier', [Type, :void])
+  end
+
+  # = Type specifiers
+  module Types
+    class Int < Type
+    end
+
+    class Short < Type
+    end
+
+    class Char < Type
+    end
+
+    class Float < Type
+    end
+
+    class Double < Type
+    end
+
+    # _Bool
+    class Bool < Type
+    end
+
+    # _Complex
+    class Complex < Type
+    end
+
+    class Pointer < Type
+      attr_reader :pointee
+
+      def initialize(pointee)
+        @pointee = Syntax::Type.ensure(pointee)
+      end
+    end
   end
 
   # 6.4.4.1 Integer constants
@@ -73,9 +138,6 @@ module Chelsy
 
   # = 6.8 Statements and blocks
 
-  class Stmt < Element
-  end
-
   # == 6.8.3 Expression and null statements
 
   # A null statement (consisting of just a semicolon) performs no operations.
@@ -87,6 +149,74 @@ module Chelsy
 
     def initialize(expr)
       @expr = Syntax::Expr.ensure(expr)
+    end
+  end
+
+  # == 6.8.2 Compound statement
+  module Syntax
+    BlockItem = Any.new('BlockItem', [Stmt, Declaration])
+  end
+
+  class Block < Stmt
+    include Enumerable
+
+    extend Forwardable
+    def_delegators :@items, :size, :each
+
+    def initialize()
+      @items = []
+    end
+
+    # Append `node` to block item list
+    #
+    #   - Implicit convertion from Expr to ExprStmt
+    def <<(node)
+      item = case node
+             when Expr
+               ExprStmt.new(node)
+             else
+               node
+             end
+      @items << Syntax::BlockItem.ensure(item)
+    end
+  end
+
+  # = 6.9 External definitions
+
+  # == 6.9.1 Function definition
+
+  # Param-List ::
+  #     [] |
+  #     [:void] |
+  #     [Param] |
+  #     [Param, ..., :"..."]
+  class Param < Element
+    attr_reader :name, :type
+
+    def initialize(name, type, register: false)
+      @name = Syntax::Ident.ensure(name)
+      @type = Syntax::Type.ensure(type)
+
+      super
+    end
+  end
+
+  module Syntax
+    Param = Any.new('Parameter', [Param, :void, :"..."])
+  end
+
+  class Function < Definition
+    attr_reader :name, :return_type, :params, :body
+
+    def initialize(name, return_type, params, inline: false, **rest, &block)
+      @name = Syntax::Ident.ensure(name)
+      @return_type = Syntax::Type.ensure(return_type)
+      @params = params.map {|p| Syntax::Param.ensure(p) }
+
+      @body = Block.new
+      block.call(@body)
+
+      super(**rest)
     end
   end
 
--- test/ast_test.rb
+++ test/ast_test.rb
@@ -3,6 +3,17 @@ require 'test_helper'
 class Chelsy::ASTTest < Minitest::Test
   include Chelsy
 
+  def test_function_definitions
+    f = Function.new(:f, :void, []) do |b|
+      b << FunctionCall.new(:printf, [Constant::String.new("Hello, World!\n")])
+    end
+
+    assert f
+    assert_equal :f, f.name
+    assert_equal :void, f.return_type
+    assert ExprStmt === f.body.first
+  end
+
   # == Bad arguments
 
   def test_function_call
@@ -18,6 +29,16 @@ class Chelsy::ASTTest < Minitest::Test
   def test_expr_stmt
     assert_raises(ArgumentError) do
       ExprStmt.new(EmptyStmt.new)
+    end
+  end
+
+  def test_function_param
+    assert_raises(ArgumentError) do
+      Param.new(EmptyStmt.new, Types::Int.new)
+    end
+
+    assert_raises(ArgumentError) do
+      Param.new(:a, 1000)
     end
   end
 
