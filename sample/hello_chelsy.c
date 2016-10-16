67ab3746cb2a9a0891b689ff2a6db97a4e588d47
Takanori Ishikawa <takanori.ishikawa@gmail.com>
2016-10-16 14:32:57 +0900

    Add "Hello, Chelsy" sample

---
+++ sample/hello_chelsy.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello, Chelsy!\n");
+    return 0;
+}
---
+++ sample/hello_chelsy.rb
@@ -0,0 +1,15 @@
+require 'chelsy'
+
+include Chelsy
+
+doc = Document.new
+
+doc.fragments << Directive::Include.new("stdio.h", system: true)
+doc.fragments << ''
+
+doc << Function.new(:main, Type::Int.new, [:void]) do |b|
+  b << FunctionCall.new(:printf, [Constant::String.new("Hello, Chelsy!\n")])
+  b << Return.new(Constant::Int.new(0))
+end
+
+puts Translator.new.translate(doc)
