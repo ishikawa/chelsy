3e1611a0fde5b4899096fd04fbb342ab57ac1e12
Takanori Ishikawa <takanori.ishikawa@gmail.com>
2016-11-23 13:22:30 +0900

    Add new example

---
+++ sample/temperature.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+ * Print Fahrenheit to Celsius table
+ * (fahr = 0, 20, ..., 300)
+ */
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
+int main(int argc, const char **argv) {
+  for (int fahr = LOWER; fahr <= UPPER; fahr += STEP) {
+    int celsius = 5 * (fahr - 32) / 9;
+    printf("%d\t%d\n", fahr, celsius);
+  }
+
+  return 0;
+}
