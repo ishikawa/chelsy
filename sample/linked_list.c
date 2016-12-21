214f2e2433d29ec87b8e9d19eda29940a3c5c052
Takanori Ishikawa <takanori.ishikawa@gmail.com>
2016-12-22 00:55:12 +0900

    Add Linked list c program

---
+++ sample/linked_list.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * A linked list node
+ */
+typedef struct node {
+  int value;
+  struct node *next;
+} node_t;
+
+int main(void) {
+  // Initializing a linked list
+  node_t *head = malloc(sizeof(node_t));
+
+  if (head == NULL) {
+    exit(1);
+  }
+
+  head->value = 1;
+  head->next = malloc(sizeof(node_t));
+
+  if (head->next == NULL) {
+    exit(1);
+  }
+
+  head->next->value = 2;
+  head->next->next = NULL;
+
+  // Iterating over a list
+  node_t *current = head;
+
+  while (current != NULL) {
+    printf("%d\n", current->value);
+    current = current->next;
+  }
+
+  return 0;
+}
